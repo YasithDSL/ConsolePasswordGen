b0ed3bb9bd8a2a8c03d7227d60950d60b6d57156
Yasith <42208881+YasithDSL@users.noreply.github.com>
2021-07-11 13:42:41 +0100

    v1.0
    First fully functional application version.

---
+++ PasswordGen/PasswordGen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PasswordGen
+{
+    class PasswordGen
+    {
+        const string lowerChars = "abcdefghijklmnopqrstuvwxyz";
+        const string upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const string specialChars = "!\"£$%^&*(){}[]@#\'.,\\?/";
+
+        public static string Generate(int length, bool specialCharsAllowed, bool letterBias)
+        {
+            string generatedPsswd = "";
+            Random rnd = new Random();
+            for(int i = 0; i < length; i++)
+            {
+                int nextChar = specialCharsAllowed ? (letterBias ? rnd.Next(5) : rnd.Next(3)) : rnd.Next(2);
+                switch(nextChar)
+                {
+                    case 0:
+                    case 3:
+                        int pos = rnd.Next(26);
+                        generatedPsswd += lowerChars[pos];
+                        break;
+                    case 1:
+                    case 4:
+                        int pos2 = rnd.Next(26);
+                        generatedPsswd += upperChars[pos2];
+                        break;
+                    case 2:
+                        int pos3 = rnd.Next(specialChars.Length);
+                        generatedPsswd += specialChars[pos3];
+                        break;
+                }
+            }
+            return generatedPsswd;
+        }
+    }
+}
---
+++ PasswordGen/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PasswordGen
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("Passowrd Generator");
+            Console.WriteLine("1. Generate a new password without saving to clipboard");
+            Console.WriteLine("2. Generate a new password saving to clipboard");
+            Console.WriteLine("\n3. Exit");
+            int input = Convert.ToInt32(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    Gen(false);
+                    Menu();
+                    break;
+                case 2:
+                    Gen(true);
+                    Menu();
+                    break;
+                case 3:
+                    return;
+                default:
+                    Menu();
+                    break;
+            }
+        }
+
+        static void Gen(bool clipboard)
+        {
+            bool acceptInput = false;
+            int length = 1;
+            while(!acceptInput)
+            {
+                try
+                {
+                    Console.WriteLine("\nEnter desired length of password: ");
+                    length = Convert.ToInt32(Console.ReadLine());
+                    acceptInput = length > 0;
+                }
+                catch
+                {
+                    Console.WriteLine("Input was not a positive integer. Please try again.");
+                }
+            }
+            Console.WriteLine("Allow special characters: [Y/N] (any other input will result in false)");
+            bool allowSpecialChars = Console.ReadLine().ToUpper() == "Y" ? true : false;
+            bool bias = false;
+            if (allowSpecialChars)
+            {
+                Console.WriteLine("Allow lower chance of special characters appearing: [Y/N] (any other input will result in false)");
+                bias = Console.ReadLine().ToUpper() == "Y";
+            }
+            string generatedPassword = PasswordGen.Generate(length, allowSpecialChars, bias);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"\n{generatedPassword}\n");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            if(clipboard)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Clipboard.SetText(generatedPassword);
+                Console.WriteLine("Coppied to clipboard!\n");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+        }
+    }
+}
